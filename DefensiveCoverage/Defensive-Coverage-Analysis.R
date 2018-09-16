5ba783e9367ff7c71920e1f431fbdc06b2b9bafb
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-09-15 23:33:06 -0400

    refactor R

--- DefensiveCoverage/Defensive-Coverage-Analysis.R
+++ DefensiveCoverage/Defensive-Coverage-Analysis.R
@@ -54,5 +54,31 @@ plot = ggplot(df,aes(x=Coverage,fill=Category)) +
   theme(legend.position="right",legend.title = element_blank())+theme(
     axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm")),
     axis.title.y = element_text(margin = unit(c(0, 3, 0, 0), "mm"), angle = 90))
-
 plot
+
+# Gaussian analysis of curves
+mean_make = mean(Shots$Defensive.Coverage.Normalized[make])
+sem_make= sd(Shots$Defensive.Coverage.Normalized[make])/sqrt(length(make))
+
+mean_miss = mean(Shots$Defensive.Coverage.Normalized[miss])
+sem_miss = sd(Shots$Defensive.Coverage.Normalized[miss])/sqrt(length(miss))
+
+# Worst 10 players with respect to Normalized Defensive Coverages
+ranking_worst = sort(Shots$Defensive.Coverage.Normalized[make],decreasing=F)
+players_worst = NULL
+for (i in 1:10){
+  players_worst[i] = which(Shots$Defensive.Coverage.Normalized==ranking_worst[i])
+}
+players_worst = Shots$PLAYER_NAME[players_worst]
+players_worst
+ranking_worst[1:10]
+
+# Best 10 players with respect to Normalized Defensive Coverages
+ranking_best = sort(Shots$Defensive.Coverage.Normalized[make],decreasing=T)
+players_best = NULL
+for (i in 1:10){
+  players_best[i] = which(Shots$Defensive.Coverage.Normalized==ranking_best[i])
+}
+players_best = Shots$PLAYER_NAME[players_best]
+players_best
+ranking_best[1:10]
